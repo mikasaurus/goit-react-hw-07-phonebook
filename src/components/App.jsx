73e275eea0063debf744a6937ab4af028e446ba4
mika <dominikanajda@gmail.com>
2023-11-14 22:50:31 +0100

    still nope

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,26 @@ import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
 import { FilterContact } from './FilterContact/FilterContact';
 import css from './App.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { getError, getLoading } from 'redux/selectors';
+import { useEffect } from 'react';
+import { fetchContacts } from 'redux/operations';
+import { Loader } from './Loader/Loader';
 
 export const App = () => {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(getLoading);
+  const error = useSelector(getError);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
     <div>
       <h2 className={css.header}>Phonebook</h2>
       <ContactForm />
+      <Loader />
       <h3 className={css.header}>Contacts</h3>
       <FilterContact />
       <ContactList />
--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,9 +1,9 @@
 import { createSelector } from '@reduxjs/toolkit';
 
 export const getContacts = state => state.contacts.contacts;
-export const getFilter = state => state.contacts.filter;
-export const getError = state => state.contact.error;
-export const isLoading = state => state.contact.isLoading;
+export const getFilter = state => state.filter;
+export const getLoading = state => state.contacts.isLoading;
+export const getError = state => state.contacts.error;
 
 export const showContacts = createSelector(
   [getContacts, getFilter],
